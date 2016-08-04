0f0496eaba2e4fca699bd9bea2825ea49985ef37
Robert Ruiz <Robert.S.Ruiz@gmail.com>
2016-08-04 18:13:33 -0400

    progress in linking play, creating form, etc.

--- web.rb
+++ web.rb
@@ -1,7 +1,28 @@
 require "sinatra"
 require 'haml'
 require_relative "mastermind"
+require "sinatra/reloader" if development?
 
 get "/" do
-  "Welcome to MasterMind"
+  haml :welcome
+end
+
+get "/play" do
+  haml :play
+end
+
+post "/guess" do
+  haml :guess
+end
+
+get "/uncle" do
+  haml :uncle
+end
+
+get "/lose" do
+  haml :lose
+end
+
+get "/win" do
+  haml :win
 end
