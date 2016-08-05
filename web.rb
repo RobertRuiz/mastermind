31e5898e506cee1e09bcc3b8a9c1e536cbcdcc1e
Robert Ruiz <Robert.S.Ruiz@gmail.com>
2016-08-05 18:16:50 -0400

    Got the color picker form and submit button working

--- web.rb
+++ web.rb
@@ -3,6 +3,8 @@ require 'haml'
 require_relative "mastermind"
 require "sinatra/reloader" if development?
 
+$mastermind = MasterMind.new
+
 get "/" do
   haml :welcome
 end
@@ -12,7 +14,27 @@ get "/play" do
 end
 
 post "/guess" do
-  haml :guess
+  # Get the colors from the form
+  # Robert writes code here to pull the colors from the #submit button on the play URL
+  # color1, color2, color3, color4
+  # params[guess.color1]
+  # params[guess.color2]
+  # params[guess.color3]
+  # params[guess.color4]
+  # use "p" to output them to the terminal for debugging
+  # p __________
+  # p guess[]
+  color1 = params["color1"]
+  color2 = params["color2"]
+  color3 = params["color3"]
+  color4 = params["color4"]
+
+  p color1
+  p color2
+  p color3
+  p color4
+  # Go back to play
+  redirect '/play'
 end
 
 get "/uncle" do
